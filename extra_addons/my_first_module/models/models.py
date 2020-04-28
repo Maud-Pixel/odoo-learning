043aa13d3afb750483a20b2628c70098b8cdf124
Maud Leleux <leleuxmaud@gmail.com>
2020-04-28 15:00:04 +0200

    functions

--- extra_addons/my_first_module/models/models.py
+++ extra_addons/my_first_module/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 
 
 class Car(models.Model):
-    _name = "Car.car"
+    _name = "car.car"
 
     name = fields.Char(string="Nom")
     horse_power = fields.Integer(string="Puissance")
@@ -14,38 +14,27 @@ class Car(models.Model):
     parking_id = fields.Many2one("parking.parking", string="Parking")
     feature_ids = fields.Many2many("car.feature", string="Option")
     total_speed = fields.Integer(string="Speed total", compute="get_total_speed")
+    message = fields.Char(string="Message", compute="say_hello")
 
     def get_total_speed(self):
         print('Nom', self.name)
         print("Puissance", self.horse_power)
         self.total_speed = self.horse_power * 20
 
+    def say_hello(self, driver_id):
+        print(self.driver_id)
+        print(self.driver_id.name)
+        self.message("say hello" + driver_id.name)
+
 
 class Parking(models.Model):
     _name = "parking.parking"
 
     name = fields.Char(string="Nom")
-   #n car_ids = fields.Many2one("car.car", "parking_id", string="Cars")
+    car_ids = fields.Many2one("car.car", string="Cars")
 
 
 class Carfeatures(models.Model):
     _name = "car.feature"
 
     name = fields.Char(string="Nom")
-
-
-
-
-# class my_first_module(models.Model):
-#     _name = 'my_first_module.my_first_module'
-#     _description = 'my_first_module.my_first_module'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
