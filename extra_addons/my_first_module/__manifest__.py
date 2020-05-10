251038cb59aa161b7b8a522b1c9486b4f7b8ea5a
Maud Leleux <leleuxmaud@gmail.com>
2020-05-10 13:06:52 +0200

    New project to understand from A to Z

--- extra_addons/my_first_module/__manifest__.py
+++ extra_addons/my_first_module/__manifest__.py
@@ -3,33 +3,19 @@
     'name': "myFirstModule",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Manage library""",
 
     'description': """
-        Long description of module's purpose
+        Manage library book catalogue and lending
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "Maud_Pixel",
+    'website': "http://www.maud-pixel.com",
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/car.xml',
-        'views/parking.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'application': True,
+    'data': ['security/library_security.xml',
+             'security/ir.model.access.csv',
+             'views/library_menu.xml']
 }
